597ef8cef201e696131ab30c0e70ed83000e8f53
sat0ma014 <sstomakoto@protonmail.com>
2021-10-16 22:12:47 +0900

    test of nextui

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
+import { CssBaseline } from '@nextui-org/react';
 import Layout from '@/components/layout';
 
 export default function MyApp({ Component, pageProps }) {
@@ -8,6 +9,7 @@ export default function MyApp({ Component, pageProps }) {
       <Head>
         <title>Next.js + MongoDB App</title>
       </Head>
+      <CssBaseline />
       <Component {...pageProps} />
     </Layout>
   );
